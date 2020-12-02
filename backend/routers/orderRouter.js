fd76b41a1b1420f6a14f30163939e3016e374144
Nam Cao <namcao26102000@gmail.com>
2020-12-02 16:25:06 +0700

    add roter pay

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -39,4 +39,22 @@ orderRouter.get('/:id', isAuth, expressAsyncHandler(async (req, res) => {
     res.status(200).send(order);
 }))
 
+orderRouter.put('/:id/pay', isAuth, expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address
+        };
+        const updatedOrder = await order.save();
+        res.send({ message: 'Order Paid', order: updatedOrder });
+    } else {
+        res.status(404).send({ message: 'Khong tim thay order' })
+    }
+}))
+
 export default orderRouter;
